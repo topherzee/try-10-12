ca68e8752705124ffb1536e0906c47d728b886f7
czimmermann <christopher.zimmermann@magnolia-cms.com>
2021-10-29 16:45:08 +0200

    Good card dragging

--- frontend/simple/src/components/CardTech.js
+++ frontend/simple/src/components/CardTech.js
@@ -66,20 +66,23 @@ function CardTech(props){
         // Determine rectangle on screen
         const hoverBoundingRect = ref.current?.getBoundingClientRect();
         // Get vertical middle
-        const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+        const hoverMiddleX = (hoverBoundingRect.right - hoverBoundingRect.left) / 2;
         // Determine mouse position
         const clientOffset = monitor.getClientOffset();
         // Get pixels to the top
-        const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+        const hoverClientX = clientOffset.x - hoverBoundingRect.left;
         // Only perform the move when the mouse has crossed half of the items height
         // When dragging downwards, only move when the cursor is below 50%
         // When dragging upwards, only move when the cursor is above 50%
+        //console.log(`drag ${dragIndex} ${hoverIndex}`)
+        console.log(`drag ${hoverMiddleX} ${hoverClientX}`)
+
         // Dragging downwards
-        if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        if (dragIndex < hoverIndex && hoverClientX < hoverMiddleX) {
             return;
         }
         // Dragging upwards
-        if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        if (dragIndex > hoverIndex && hoverClientX > hoverMiddleX) {
             return;
         }
         // Time to actually perform the action
