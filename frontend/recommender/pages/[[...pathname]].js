8dec7dcf684377c7f1a6bb12d7ccd43b6c74d826
czimmermann <christopher.zimmermann@magnolia-cms.com>
2022-10-22 17:10:36 +0200

    preview mode start

--- frontend/recommender/pages/[[...pathname]].js
+++ frontend/recommender/pages/[[...pathname]].js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import * as ReactDOMServer from "react-dom/server";
 
+import { languages, getCurrentLanguage, setURLSearchParams } from "../utils";
+
 import { EditablePage } from "@magnolia/react-editor";
 import Basic from "../templates/pages/Basic";
 import ReviewGrid from "../templates/components/ReviewGrid";
@@ -48,7 +50,7 @@ const fetchAllPages = async () => {
 export async function getStaticPaths() {
   console.log("Main page.getStaticPaths() Start. ");
   const posts = await fetchAllPages();
-  console.log("****** json2:" + JSON.stringify(posts, null, 2));
+  //console.log("****** json2:" + JSON.stringify(posts, null, 2));
   const paths = posts.map((post) => ({
     params: { pathname: ["recommend", post["@name"]] },
   }));
@@ -67,42 +69,96 @@ export async function getStaticPaths() {
 
 // export async function getServerSideProps(context) {
 export async function getStaticProps(context) {
-  var params = context.params;
-  console.log("Main page. getStaticProps Start. ");
-  //console.log("params: " + JSON.stringify(params, null, 2));
+  console.log(
+    "PREVIEW: " +
+      context.preview +
+      "-- " +
+      (context.preview ? context.previewData.query.slug : "")
+  );
+  var resolvedUrl = context.preview
+    ? context.previewData.query.slug
+    : context.params.pathname
+    ? "/" + context.params.pathname.join("/")
+    : "";
+  resolvedUrl = resolvedUrl.replace(new RegExp(".*" + nodeName), "");
+  console.log("resolvedUrl: " + resolvedUrl);
+  const currentLanguage = getCurrentLanguage(resolvedUrl);
+  const isDefaultLanguage = currentLanguage === languages[0];
+  const isPagesApp = context.previewData?.query?.mgnlPreview || null;
 
-  console.log("pathname:" + JSON.stringify(params.pathname, null, 2));
-
-  const name = params.pathname;
-  const decodedName = decodeURI(name);
-  var decodedName2 = decodedName.replace(",", "/");
-  if (decodedName2 === "undefined") {
-    decodedName2 = "";
-  }
-  console.log("decodedName2 yo: " + decodedName2);
-  decodedName2 = "/" + decodedName2;
-  decodedName2 = decodedName2.replace(new RegExp(".*" + nodeName), "");
-
-  console.log("decodedName2 B: " + decodedName2);
-
-  // const pagePath = "/" + decodedName2;
-  const pagePath = nodeName + "/" + decodedName2;
-  console.log("pagePath: " + pagePath);
-
-  const isPagesApp = true; //context.query?.mgnlPreview || null;
   let props = {
     isPagesApp,
     isPagesAppEdit: isPagesApp === "false",
-    pagePath: pagePath,
+    basename: isDefaultLanguage ? "" : "/" + currentLanguage,
   };
 
   global.mgnlInPageEditor = props.isPagesAppEdit;
 
+  // Find out page path in Magnolia
+  let pagePath = context.preview
+    ? nodeName + resolvedUrl.replace(new RegExp(".*" + nodeName), "")
+    : nodeName + resolvedUrl;
+
+  console.log("pagePath: " + pagePath);
+  // if (!isDefaultLanguage) {
+  //   pagePath = pagePath.replace("/" + currentLanguage, "");
+  // }
+  props.pagePath = pagePath;
+
   // Fetching page content
-  const url = pagesApi + props.pagePath;
+  // Fetching page content
+  const url = pagesApi + pagePath;
+  //setURLSearchParams(pagesApi + pagePath, 'lang=' + currentLanguage)
   console.log("page: " + url);
   const pagesRes = await fetch(url, H);
   props.page = await pagesRes.json();
+
+  // const pagesRes = await fetch(setURLSearchParams(pagesApi + pagePath, 'lang=' + currentLanguage));
+  // props.page = await pagesRes.json();
+
+  // Fetch template annotations only inside Magnolia WYSIWYG
+  // if (isPagesApp) {
+  //   const templateAnnotationsRes = await fetch(templateAnnotationsApi + pagePath);
+
+  //   props.templateAnnotations = await templateAnnotationsRes.json();
+  // }
+
+  // var params = context.params;
+  // console.log("Main page. getStaticProps Start. ");
+  // //console.log("params: " + JSON.stringify(params, null, 2));
+
+  // console.log("pathname:" + JSON.stringify(params.pathname, null, 2));
+
+  // const name = params.pathname;
+  // const decodedName = decodeURI(name);
+  // var decodedName2 = decodedName.replace(",", "/");
+  // if (decodedName2 === "undefined") {
+  //   decodedName2 = "";
+  // }
+  // console.log("decodedName2 yo: " + decodedName2);
+  // decodedName2 = "/" + decodedName2;
+  // decodedName2 = decodedName2.replace(new RegExp(".*" + nodeName), "");
+
+  // console.log("decodedName2 B: " + decodedName2);
+
+  // // const pagePath = "/" + decodedName2;
+  // const pagePath = nodeName + "/" + decodedName2;
+  // console.log("pagePath: " + pagePath);
+
+  // const isPagesApp = true; //context.query?.mgnlPreview || null;
+  // let props = {
+  //   isPagesApp,
+  //   isPagesAppEdit: isPagesApp === "false",
+  //   pagePath: pagePath,
+  // };
+
+  // global.mgnlInPageEditor = props.isPagesAppEdit;
+
+  // // Fetching page content
+  // const url = pagesApi + props.pagePath;
+  // console.log("page: " + url);
+  // const pagesRes = await fetch(url, H);
+  // props.page = await pagesRes.json();
 
   console.log("Main page. gSSP End." + new Date().getSeconds());
 
